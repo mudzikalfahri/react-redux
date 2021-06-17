964b46389f8b6746a3678cd783e57ffe5104c7d1
unknown <alfahrimudzik@gmail.com>
2021-06-17 17:37:06 +0700

    creating grid for homepage

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+const MenuItem = () => (
+    <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>
+              YEAR END
+          </div>
+        </div>
+    </div>
+);
+
+export default MenuItem;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './homepage.styles.scss';
+import MenuItem from '../../components/menu-item/menu-item.component';
 
 const HomePage = () => (
     <div className="homepage">
-        
+        <div className='directory-menu'>
+            <MenuItem/>
+            <MenuItem/>
+            <MenuItem/>
+            <MenuItem/>
+        </div>
     </div>
 );
 
