a9e3e1f252872280c6f525f5a8908f7140b5372c
unknown <alfahrimudzik@gmail.com>
2021-06-19 20:24:34 +0700

    creating menu toggle and link them into certain path

--- src/pages/navbar/navbar.component.jsx
+++ src/pages/navbar/navbar.component.jsx
@@ -1,24 +1,59 @@
 import React from 'react';
 import './navbar.styles.scss';
 import { ReactComponent as Arrow } from '../../assets/next-menu.svg';
+import { Link } from 'react-router-dom';
 import { ReactComponent as Cart } from '../../assets/cart-shop.svg';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 
-const NavBar = () => (
-    <nav>
-        <div className="navbar">
-        <div className="container">
-            <div className="logo">
-                revut.
+class NavBar extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            toggle:false
+        }
+}
+
+navToggle = () => {
+    this.setState({toggle:!this.state.toggle});
+};
+
+resetMenu = () => {
+    this.setState({toggle:false})
+};
+
+render(){
+    return (
+        <nav>
+            <div className="navbar">
+                <div className="menu"
+                style={(this.state.toggle) ? ({top:0}) : ({top:-150})}>
+                        <div className="menu-link">
+                            Sign In
+                        </div>
+                        <Link to = '/shop' className="menu-link" onClick={this.resetMenu}>
+                            Shop
+                        </Link>
+                        <div className="menu-link">
+                            About
+                        </div>
+                </div>
+                <div className="container">
+                    <Link to='/' className="logo" onClick={this.resetMenu}>
+                        <Logo/>
+                    </Link>
+                    <button className="dropmenu" onClick={this.navToggle}>
+                        <Arrow/>
+                    </button>
+                    <div className="cart">
+                        <Cart/>
+                    </div>
+                </div>
             </div>
-            <div className="dropmenu">
-                <Arrow/>
-            </div>
-            <div className="cart">
-                <Cart/>
-            </div>
-        </div>
-        </div>
-    </nav>
-);
+        </nav>
+    );
+}
+}
+    
+
 
 export default NavBar;
--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -1,61 +1,61 @@
 const SHOP_DATA = [
     {
       id: 1,
-      title: 'Hats',
-      routeName: 'hats',
+      title: 'T-shirt',
+      routeName: 'tshirt',
       items: [
         {
           id: 1,
-          name: 'Brown Brim',
-          imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+          name: 'REVUT Tshirt Addicted',
+          imageUrl: 'https://i.ibb.co/XxKn0mN/bantersnaps-mtsb-b-Cvr-BY-unsplash.jpg',
           price: 25
         },
         {
           id: 2,
-          name: 'Blue Beanie',
-          imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+          name: 'NO Tshirt PantsGang',
+          imageUrl: 'https://i.ibb.co/g7Dgf4Q/joeyy-lee-VMSb0m-Fhn-Q8-unsplash.jpg',
           price: 18
         },
         {
           id: 3,
-          name: 'Brown Cowboy',
-          imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+          name: 'REVUT Tshirt Cre-Heart',
+          imageUrl: 'https://i.ibb.co/pv0h5Xk/chris-lynch-Trd4o-7-OL60-unsplash.jpg',
           price: 35
         },
         {
           id: 4,
-          name: 'Grey Brim',
-          imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+          name: 'REVUT Tshirt Outcast',
+          imageUrl: 'https://i.ibb.co/N18XNkb/taylor-d-G4-Eb-o-C5i-M-unsplash.jpg',
           price: 25
         },
         {
           id: 5,
-          name: 'Green Beanie',
-          imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
+          name: 'COB Tshirt Classic',
+          imageUrl: 'https://i.ibb.co/D9qwBNX/sincerely-media-qe-Dc-KFADdp8-unsplash.jpg',
           price: 18
         },
         {
           id: 6,
-          name: 'Palm Tree Cap',
-          imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
+          name: 'BASIC Tshirt White',
+          imageUrl: 'https://i.ibb.co/YPYxWhx/hossein-hosseini-B3-Ux-Sm-BTMx-U-unsplash.jpg',
           price: 14
         },
         {
           id: 7,
-          name: 'Red Beanie',
-          imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
+          name: 'TS Tshirt Black OS',
+          imageUrl: 'https://i.ibb.co/yBtkn2m/ian-dooley-kkj9i-Kxsdh-Y-unsplash.jpg',
           price: 18
         },
         {
           id: 8,
-          name: 'Wolf Cap',
-          imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
+          name: 'REVUT Tshirt Jo',
+          imageUrl: 'https://i.ibb.co/Xx2pps3/valentin-lacoste-bqi-MUm-SAd-AQ-unsplash.jpg',
           price: 14
         },
         {
           id: 9,
-          name: 'Blue Snapback',
-          imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
+          name: 'JAP Tshirt Kanji',
+          imageUrl: 'https://i.ibb.co/mqKjsss/uji-kanggo-gumilang-s-Mn0sx-R8v2-E-unsplash.jpg',
           price: 16
         }
       ]
@@ -152,98 +152,6 @@ const SHOP_DATA = [
         }
       ]
     },
-    {
-      id: 4,
-      title: 'Womens',
-      routeName: 'womens',
-      items: [
-        {
-          id: 23,
-          name: 'Blue Tanktop',
-          imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-          price: 25
-        },
-        {
-          id: 24,
-          name: 'Floral Blouse',
-          imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-          price: 20
-        },
-        {
-          id: 25,
-          name: 'Floral Dress',
-          imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-          price: 80
-        },
-        {
-          id: 26,
-          name: 'Red Dots Dress',
-          imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-          price: 80
-        },
-        {
-          id: 27,
-          name: 'Striped Sweater',
-          imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-          price: 45
-        },
-        {
-          id: 28,
-          name: 'Yellow Track Suit',
-          imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
-          price: 135
-        },
-        {
-          id: 29,
-          name: 'White Blouse',
-          imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
-          price: 20
-        }
-      ]
-    },
-    {
-      id: 5,
-      title: 'Mens',
-      routeName: 'mens',
-      items: [
-        {
-          id: 30,
-          name: 'Camo Down Vest',
-          imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
-          price: 325
-        },
-        {
-          id: 31,
-          name: 'Floral T-shirt',
-          imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
-          price: 20
-        },
-        {
-          id: 32,
-          name: 'Black & White Longsleeve',
-          imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
-          price: 25
-        },
-        {
-          id: 33,
-          name: 'Pink T-shirt',
-          imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-          price: 25
-        },
-        {
-          id: 34,
-          name: 'Jean Long Sleeve',
-          imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-          price: 40
-        },
-        {
-          id: 35,
-          name: 'Burgundy T-shirt',
-          imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-          price: 25
-        }
-      ]
-    }
   ];
   
   export default SHOP_DATA;
