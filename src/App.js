82700f3ab55f53b8914d328b8aa2089fbc81a173
unknown <alfahrimudzik@gmail.com>
2021-06-24 20:27:28 +0700

    creating firestore to store user data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavBar from './pages/navbar/navbar.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import SignInSignUpPage from './pages/sign-in-up/sign-in-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument  } from './firebase/firebase.utils';
 import React from 'react';
 
 class App extends React.Component { 
@@ -19,8 +19,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
