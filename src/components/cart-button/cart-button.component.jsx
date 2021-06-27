3158fda81aaad15f2765ebc76470207b2bebb6d2
unknown <alfahrimudzik@gmail.com>
2021-06-27 09:06:59 +0700

    creating cart preview component

--- src/components/cart-button/cart-button.component.jsx
+++ src/components/cart-button/cart-button.component.jsx
@@ -4,22 +4,26 @@ import { ReactComponent as Cart } from '../../assets/cart-shop.svg';
 import { setToggleCart } from '../../redux/cart-button/cart-actions';
 import { connect } from 'react-redux';
 
-const CartButton = ({setToggleCart}) => {
+const CartButton = ({setToggleCart, cartItems}) => {
     return (
         <div className='cart' onClick={setToggleCart}>
             <Cart/>
             <div className="cart-num">
-                0
+                {cartItems.length}
             </div>
         </div>
     )
 }
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+  });
 
 const mapDispatchToProps = dispatch => ({
     setToggleCart: () => dispatch(setToggleCart())
   });
   
   export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(CartButton);
--- src/components/cart-preview/cart-preview.component.jsx
+++ src/components/cart-preview/cart-preview.component.jsx
@@ -4,17 +4,24 @@ import './cart-preview.styles.scss';
 import { ReactComponent as CartArrow } from '../../assets/close-arrow.svg';
 import { setToggleCart } from "../../redux/cart-button/cart-actions";
 import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartPreview = ({setToggleCart}) => {
+const CartPreview = ({setToggleCart, cartItems }) => {
     return (
         <div className='cart-preview'>
             <div className="close" onClick={setToggleCart}>
                 My Cart
                 <CartArrow/>
             </div>
-            <div className="cart-item">
-                Your shopping cart is empty.
+            <div className="cart-items">
+                {cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+                ))}
                 <hr/>
+            </div>
+            <div className="subtotal">
+                <span>Subtotal</span>
+                <span>$340</span>
             </div>
             
             <CustomButton>Checkout Items</CustomButton>
@@ -22,11 +29,15 @@ const CartPreview = ({setToggleCart}) => {
     )
 };
 
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+  });
+
 const mapDispatchToProps = dispatch => ({
     setToggleCart: () => dispatch(setToggleCart())
   });
   
   export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(CartPreview);
