76e0bc103c3b1dd45a4e2646bc0167dd1c92bf2b
unknown <alfahrimudzik@gmail.com>
2021-06-29 20:44:22 +0700

    category

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -6,10 +6,10 @@ import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
-    <h1 className='title'>{title}</h1>
+    {/* <h1 className='title'>{title}</h1> */}
     <div className='preview'>
       {items
-        .filter((item, idx) => idx < 4)
+        .filter((item, idx) => idx < 8)
         .map((item) => (
           <CollectionItem key={item.id} item={item} title={title}/>
         ))}
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,7 +8,8 @@ class ShopPage extends React.Component {
     super(props);
 
     this.state = {
-      collections: SHOP_DATA
+      collections: SHOP_DATA,
+      cat:3
     };
   }
 
@@ -17,12 +18,12 @@ class ShopPage extends React.Component {
     return (
       <div className='shop-page'>
         <div className="cathegory-list">
-          <div className="catall">All Products</div>
-          <div className="cat1">T-Shirt</div>
-          <div className="cat2">Footwear</div>
-          <div className="cat3">Pants</div>
+          <div className="catall" onClick={()=>this.setState({cat:3})}>All Products</div>
+          <div className="cat1" onClick={()=>this.setState({cat:0})}>T-Shirt</div>
+          <div className="cat2" onClick={()=>this.setState({cat:1})}>Footwear</div>
+          <div className="cat3" onClick={()=>this.setState({cat:2})}>Jackets</div>
         </div>
-        {collections.map(({ id, ...otherCollectionProps }) => (
+        {collections.filter((collect,ids) => this.state.cat<3 ? ids===this.state.cat : true).map(({ id, ...otherCollectionProps }) => (
           <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
       </div>
